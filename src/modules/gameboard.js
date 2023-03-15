8bc06516a692b9dcf3f6afa7b1707d62d1a8da91
Dragesia <brkgzlsy@gmail.com>
2023-03-15 15:04:51 +0300

    edit placeShip and add receiveAttack to gameboard

--- src/modules/gameboard.js
+++ src/modules/gameboard.js
@@ -12,22 +12,30 @@ export default function createGameboard() {
                     [0,0,0,0,0,0,0,0,0,0],
                     [0,0,0,0,0,0,0,0,0,0],
                     [0,0,0,0,0,0,0,0,0,0]],
+        missed: [],
         placeShip(x, y, axis, shipLength) {
             let ship = createShip(shipLength);
             if (axis == 'vertical') {
+                if (y + shipLength > 10) y = 10 - shipLength;
                 for (let i = y; i < y + shipLength; i++) {
                     this.gameboard[x][i] = ship;
                 }
             } else if (axis == 'horizontal') {
+                if (x + shipLength > 10) x = 10 - shipLength;
                 for (let i = x; i < x + shipLength; i++) {
                     this.gameboard[i][y] = ship;
                 }
             }
+        },
+        receiveAttack(x, y) {
+            if (this.gameboard[x][y] != 0) this.gameboard[x][y].hit++;
+            else if (!this.missed.includes([x, y])) this.missed.push([x, y]);
         }
     }
 }
 
 let player = createGameboard();
 
-player.placeShip(1, 2, 'horizontal', 4);
+player.placeShip(1, 9, 'vertical', 4);
+
 console.log(player.gameboard);

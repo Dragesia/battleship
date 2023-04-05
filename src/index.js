68c36941392f88e77de6746a501018c1dacff1a0
Dragesia <brkgzlsy@gmail.com>
2023-04-05 20:49:34 +0300

    add stats

--- src/index.js
+++ src/index.js
@@ -20,9 +20,9 @@ const enemyWaters = document.querySelectorAll('.computer-board > .water');
 const winner = document.querySelector('.winner-container');
 const result = document.querySelector('.result');
 const playAgain = document.querySelector('.play-again');
-// const totalShots = document.querySelector('.total-shots');
-// const missedShots = document.querySelector('.missed-shots');
-// const accuracy = document.querySelector('.accuracy');
+const totalShots = document.querySelector('.total-shots');
+const missedShots = document.querySelector('.missed-shots');
+const accuracy = document.querySelector('.accuracy');
 playAgain.onclick = () => window.location.reload();
 enemyWaters.forEach(enemyWater => {
     let index = [...enemyWater.parentElement.children].indexOf(enemyWater);
@@ -36,7 +36,10 @@ enemyWaters.forEach(enemyWater => {
             winner.style.display = 'flex';
             result.innerHTML = 'win';
             result.style.color = 'darkgreen';
-
+            totalShots.innerHTML = player.game.totalShots;
+            missedShots.innerHTML = player.game.missed.length;
+            console.log(player.game.missed);
+            accuracy.innerHTML = ( ( (player.game.totalShots-player.game.missed.length) / player.game.totalShots ) * 100 ).toFixed(2) + '%';
             console.log('You win!');
         }
         computer.playRandom(player);
@@ -44,7 +47,9 @@ enemyWaters.forEach(enemyWater => {
             winner.style.display = 'flex';
             result.innerHTML = 'lost';
             result.style.color = 'darkred';
-
+            totalShots.innerHTML = player.game.totalShots;
+            missedShots.innerHTML = player.game.missed.length;
+            accuracy.innerHTML = ( ( (player.game.totalShots-player.game.missed.length) / player.game.totalShots ) * 100 ).toFixed(2) + '%';
             console.log('You lost!');
         }
     }
